8e0a77de601ffe856a32e8f279d23d0888317b61
MustafaEgeAtay <megeatay@gmail.com>
2023-10-21 16:29:16 +0300

    Update ultrasonic__lcd.ino

--- 06_Experimental/ultrasonic__lcd/ultrasonic__lcd.ino
+++ 06_Experimental/ultrasonic__lcd/ultrasonic__lcd.ino
@@ -10,11 +10,10 @@
 #define ECHO_PIN     11  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 
-int ultrasonic_o;
+const int buttonPin = 7;
+int buttonState = 0;
+unsigned long ultrasonic_o;
 int height=30 ;
-
-bool state=true;
-
 
 
 
@@ -28,40 +27,51 @@ void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
   lcd.begin();
   lcd.backlight();
+  pinMode(buttonPin, INPUT);
 }
 
 void loop() {
-  
+
   delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
   Serial.print("Yükseklik: ");
   Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
   Serial.println("cm");
 
-  ultrasonic_o= sonar.ping_cm();
+  delay(50);
+  ultrasonic_o = sonar.ping_cm();
   Serial.println(ultrasonic_o);
+
+  buttonState = digitalRead(buttonPin);
+
+
+
   
 
-    if (state){
+    if (buttonState == HIGH){
 
   lcd.setCursor(0,0);
   lcd.print("Yukseklik:");
-    lcd.setCursor(3,1);
+  lcd.setCursor(3,1);
   lcd.print("cm");
   lcd.setCursor(0, 1); // Set cursor to the first column of the second row
   lcd.print("  "); // Clear the second row by printing spaces
   lcd.setCursor(0, 1); // Set cursor to the first column of the second row
-  lcd.print(sonar.ping_cm()); // Print the new number on the second row
+  delay(50);
+  lcd.print(ultrasonic_o); // Print the new number on the second row
   delay(500);
 
 
 
   } else {
 
+  lcd.clear();  
   lcd.setCursor(0,0);
   lcd.print("istenen:");
   lcd.setCursor(0,1);
   lcd.print(height);
   
   }
+
+
 
 }
