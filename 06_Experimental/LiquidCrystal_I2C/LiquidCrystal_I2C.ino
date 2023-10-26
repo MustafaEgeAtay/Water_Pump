10cf3d908b8f1cfee7f7a4c3af9e6af9a386cf93
SultanPalamut <113286115+SultanPalamut@users.noreply.github.com>
2023-10-26 15:26:20 +0300

    Update LiquidCrystal_I2C.ino

--- 06_Experimental/LiquidCrystal_I2C/LiquidCrystal_I2C.ino
+++ 06_Experimental/LiquidCrystal_I2C/LiquidCrystal_I2C.ino
@@ -1,46 +1,16 @@
-/*
-   Lezzetli Robot Tarifleri
-   Arduino İle Nasıl Kullanılır
-   Kızılötesi Kumanda
-   VİDEO >>> https://lezzetlirobottarifleri.com/tv-kumandasi-ile-arduino-kontrolu
-*/
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-#include <IRremote.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-IRrecv irrecv(6);
-
-decode_results results;
-
-#define BUTON1 0xFFA25D
-#define BUTON2 0xFF629D
-#define BUTON3 0xFFE21D
-#define BUTON4 0xFF22DD
-#define BUTON5 0xFF02FD
-#define BUTON6 0xFFC23D
-#define BUTON7 0xFFE01F
-#define BUTON8 0xFFA857
-#define BUTON9 0xFF906F
-#define YILDIZ 0xFF6897
-#define BUTON0 0xFF9867
-#define YUKARI 0xFF18E7
-#define SOL    0xFF10EF
-#define OK     0xFF38C7
-#define SAG    0xFF5AA5
-#define ASAGI  0xFF4AB5
-
-
-void setup() 
-{
-  Serial.begin(9600);
-  irrecv.enableIRIn();
+void setup() {
+  lcd.begin();
 }
 
-void loop() 
-{
-  if (irrecv.decode(&results))
-  {
-    Serial.println(results.value, HEX);
-    irrecv.resume();
-  }
-  
+void loop() {
+  lcd.home();
+  lcd.setCursor(0, 0);
+  lcd.print("LEZZETLI ROBOT");
+  lcd.setCursor(4, 1);
+  lcd.print("TARIFLERI");
 }
