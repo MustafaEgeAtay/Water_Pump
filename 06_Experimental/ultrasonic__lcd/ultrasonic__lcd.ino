46322d5d1ec303093d197cf7077e1d7935ebbd98
MustafaEgeAtay <megeatay@gmail.com>
2023-10-26 15:05:35 +0300

    Update ultrasonic__lcd.ino

--- 06_Experimental/ultrasonic__lcd/ultrasonic__lcd.ino
+++ 06_Experimental/ultrasonic__lcd/ultrasonic__lcd.ino
@@ -11,6 +11,14 @@
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
 #define POTPIN A0
 
+#define LEVER_SWITCH_PIN1 2
+#define LEVER_SWITCH_PIN2 3
+#define LEVER_SWITCH_PIN3 4
+
+int pressSwitch[2] ;
+int mode;
+int previousMode;
+
 
 int buttonState = 0;
 unsigned long ultrasonic_o;
@@ -39,90 +47,71 @@ void loop() {
   deger = analogRead(POTPIN)/56;
   delay(1); 
 
- 
-  Serial.println(deger);
-  
-
-  delay(50);                     // Wait 50ms between pings (about 20 pings/sec). 29ms should be the shortest delay between pings.
-  //Serial.print("Yükseklik: ");
-  //Serial.print(sonar.ping_cm()); // Send ping, get distance in cm and print result (0 = outside set distance range)
-  //Serial.println("cm");
-
   delay(50);
   ultrasonic_o = sonar.ping_cm();
- // Serial.println(ultrasonic_o);
 
-  
-
-
-
-  
-
-  /*  if (deger == previousDeger){
-
-  lcd.setCursor(0,0);
-  lcd.print("Yukseklik:");
-  lcd.setCursor(3,1);
-  lcd.print("cm");
-  lcd.setCursor(0, 1); // Set cursor to the first column of the second row
-  lcd.print("  "); // Clear the second row by printing spaces
-  lcd.setCursor(0, 1); // Set cursor to the first column of the second row
-  delay(50);
-  lcd.print(ultrasonic_o); // Print the new number on the second row
-  delay(500);
-
- changeTime = millis();
-
-  } else {
-
-  lcd.setCursor(0,0);
+ if (deger != previousDeger) {
+  // The target distance (istenen) has changed
+ 
+  lcd.setCursor(0, 0);
   lcd.print("istenen:   ");
-  lcd.setCursor(3,1);
+  lcd.setCursor(3, 1);
   lcd.print("cm");
-  lcd.setCursor(0, 1); // Set cursor to the first column of the second row
-  lcd.print("  "); // Clear the second row by printing spaces
-  lcd.setCursor(0, 1); // Set cursor to the first column of the second row
+  lcd.setCursor(0, 1);
+  lcd.print("  ");
+  lcd.setCursor(0, 1);
   delay(50);
-  lcd.print(deger); // Print the new number on the second row
-  delay(1000);
+  lcd.print(deger);
+
   previousDeger = deger;
+  previousMode = mode;
   
-  }*/
 
+  // Record the time of the change
+  changeTime = millis();
+} else if (mode != previousMode) {
+  // The mode has changed
+  lcd.setCursor(0, 0);
+  lcd.print("Mod:      ");
+  lcd.setCursor(0, 1);
+  lcd.print("       ");
+  delay(50);
+  lcd.setCursor(0, 1);
+  lcd.print(mode);
 
-  
-  if (deger != previousDeger) {
-    // The target distance has changed
+  previousDeger = deger;
+  previousMode = mode;
+
+  // Record the time of the change
+  changeTime = millis();
+} else {
+  if (millis() - changeTime >= waitTime) {
+    // Wait for 3 seconds before exiting the if block
     lcd.setCursor(0, 0);
-    lcd.print("istenen:   ");
+    lcd.print("Yukseklik:");
     lcd.setCursor(3, 1);
     lcd.print("cm");
     lcd.setCursor(0, 1);
     lcd.print("  ");
     lcd.setCursor(0, 1);
-    delay(50);
-    lcd.print(deger);
-    previousDeger = deger;
-
-    // Record the time of the change
-    changeTime = millis();
-  } else {
-    if (millis() - changeTime >= waitTime) {
-      // Wait for 3 seconds before exiting the if block
-      lcd.setCursor(0, 0);
-      lcd.print("Yukseklik:");
-      lcd.setCursor(3, 1);
-      lcd.print("cm");
-      lcd.setCursor(0, 1);
-      lcd.print("  ");
-      lcd.setCursor(0, 1);
-      delay(50);
-      lcd.print(ultrasonic_o);
-    }
+    delay(500);
+    lcd.print(ultrasonic_o);
   }
+}
 
-  
 
+pinMode(LEVER_SWITCH_PIN1,INPUT);
+pinMode(LEVER_SWITCH_PIN2,INPUT);
+pinMode(LEVER_SWITCH_PIN3,INPUT);
+pressSwitch[2] = digitalRead(LEVER_SWITCH_PIN1);
+pressSwitch[1] = digitalRead(LEVER_SWITCH_PIN2);
+pressSwitch[0] = digitalRead(LEVER_SWITCH_PIN3);
+delay(10);
+mode = 4*pressSwitch[2] + 2*pressSwitch[1] + pressSwitch[0];
+
+Serial.println(mode);
+
+ 
 }
 
 
