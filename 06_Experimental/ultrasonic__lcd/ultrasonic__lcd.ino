794c8daae1144f4710e4acb4b2a582020964fb9d
MustafaEgeAtay <megeatay@gmail.com>
2023-11-13 08:35:30 +0300

    Update ultrasonic__lcd.ino

--- 06_Experimental/ultrasonic__lcd/ultrasonic__lcd.ino
+++ 06_Experimental/ultrasonic__lcd/ultrasonic__lcd.ino
@@ -1,6 +1,5 @@
 //ultraonic sensör ve lcd ekran ayarlari
 
-
 #include <stdio.h>
 #include <NewPing.h>
 #include <Wire.h> 
@@ -15,7 +14,7 @@
 #define LEVER_SWITCH_PIN2 3
 #define LEVER_SWITCH_PIN3 4
 
-int pressSwitch[2] ;
+int pressSwitch[3] ;
 int mode;
 int previousMode;
 
@@ -27,6 +26,11 @@ int previousDeger = 0;
 unsigned long changeTime = 0; // Variable to store the time of the last change
 unsigned long waitTime = 3000;
 
+enum menu_state{
+    current_distance = 0,
+    wanted_distance = 1,
+    current_mode = 2
+}menu_current_state;
 
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
@@ -39,53 +43,87 @@ void setup() {
   Serial.begin(115200); // Open serial monitor at 115200 baud to see ping results.
   lcd.begin();
   lcd.backlight();
+
+  pinMode(LEVER_SWITCH_PIN1,INPUT);
+  pinMode(LEVER_SWITCH_PIN2,INPUT);
+  pinMode(LEVER_SWITCH_PIN3,INPUT);
+
+  menu_current_state = current_distance;
   
 }
 
 void loop() {
 
+  // -------------- Read Variables --------------
+
   deger = analogRead(POTPIN)/56;
-  delay(1); 
-
-  delay(50);
-  ultrasonic_o = sonar.ping_cm();
-
- if (deger != previousDeger) {
-  // The target distance (istenen) has changed
- 
-  lcd.setCursor(0, 0);
-  lcd.print("istenen:   ");
-  lcd.setCursor(3, 1);
-  lcd.print("cm");
-  lcd.setCursor(0, 1);
-  lcd.print("  ");
-  lcd.setCursor(0, 1);
-  delay(50);
-  lcd.print(deger);
-
-  previousDeger = deger;
-  previousMode = mode;
   
+  ultrasonic_o = sonar.ping_cm();
+ 
 
-  // Record the time of the change
-  changeTime = millis();
-} else if (mode != previousMode) {
-  // The mode has changed
-  lcd.setCursor(0, 0);
-  lcd.print("Mod:      ");
-  lcd.setCursor(0, 1);
-  lcd.print("       ");
-  delay(50);
-  lcd.setCursor(0, 1);
-  lcd.print(mode);
+  pressSwitch[2] = digitalRead(LEVER_SWITCH_PIN1);
+  pressSwitch[1] = digitalRead(LEVER_SWITCH_PIN2);
+  pressSwitch[0] = digitalRead(LEVER_SWITCH_PIN3);
+  
+  mode = 4*pressSwitch[2] + 2*pressSwitch[1] + pressSwitch[0];
 
-  previousDeger = deger;
-  previousMode = mode;
+  // -------------- Read Variables --------------
 
-  // Record the time of the change
-  changeTime = millis();
-} else {
-  if (millis() - changeTime >= waitTime) {
+  // -------------- LCD Screen Output --------------
+
+  if((deger != previousDeger) || (menu_current_state == wanted_distance)){
+    // The target distance (istenen) has changed
+   
+    lcd.setCursor(0, 0);
+    lcd.print("istenen:   ");
+    lcd.setCursor(3, 1);
+    lcd.print("cm");
+    lcd.setCursor(0, 1);
+    lcd.print("  ");
+    lcd.setCursor(0, 1);
+    delay(50);
+    lcd.print(deger);
+    delay(50);
+    
+    // Record the time of the change
+    if(deger != previousDeger ){
+        changeTime = millis();
+        menu_current_state = wanted_distance;
+        previousDeger = deger;
+    }
+    
+    if(millis() - changeTime >= waitTime){
+        menu_current_state = current_distance;
+    }
+  
+  }
+
+  if ((mode != previousMode) || (menu_current_state == current_mode)) {
+    // The mode has changed
+    lcd.setCursor(0, 0);
+    lcd.print("Mod:      ");
+    lcd.setCursor(0, 1);
+    lcd.print("       ");
+    delay(50);
+    lcd.setCursor(0, 1);
+    lcd.print(mode);
+    delay(50);
+  
+    // Record the time of the change
+    if(mode != previousMode){
+        changeTime = millis();
+        menu_current_state = current_mode;
+        previousMode = mode;
+    }
+
+    if(millis() - changeTime >= waitTime){
+        menu_current_state = current_distance;
+    }
+  
+  }
+  
+  if(menu_current_state == current_distance){
+    
     // Wait for 3 seconds before exiting the if block
     lcd.setCursor(0, 0);
     lcd.print("Yukseklik:");
@@ -94,26 +132,17 @@ void loop() {
     lcd.setCursor(0, 1);
     lcd.print("  ");
     lcd.setCursor(0, 1);
-    delay(500);
+    
+    
     lcd.print(ultrasonic_o);
+    delay(100);
+
+    
   }
+
+  // -------------- LCD Screen Output --------------
+  
+    Serial.println(mode);
+    
 }
-
-
-pinMode(LEVER_SWITCH_PIN1,INPUT);
-pinMode(LEVER_SWITCH_PIN2,INPUT);
-pinMode(LEVER_SWITCH_PIN3,INPUT);
-pressSwitch[2] = digitalRead(LEVER_SWITCH_PIN1);
-pressSwitch[1] = digitalRead(LEVER_SWITCH_PIN2);
-pressSwitch[0] = digitalRead(LEVER_SWITCH_PIN3);
-delay(10);
-mode = 4*pressSwitch[2] + 2*pressSwitch[1] + pressSwitch[0];
-
-Serial.println(mode);
-
- 
-}
-
-
-
 
